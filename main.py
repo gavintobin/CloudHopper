49f2227d89599ecba73a27ff36b2f421e91e48a4
gavintobin <gavintobin714@gmail.com>
2023-04-21 12:14:22 -0700

    line 76 start.

--- main.py
+++ main.py
@@ -22,3 +22,35 @@ game_over = False
 
 
 # Game variables
+# actual line 78 but line 25 rn till we get rest of content
+for i in range(len(platforms)):
+    block = pygame.draw.rect(screen, black, platforms[i, 0, 3])
+    block.append(block)
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_a:
+            x_change = -player_speed
+        if event.key == pygame.K_d:
+            x_change = player_speed
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_a:
+            x_change = 0
+        if event.key == pygame.K_d:
+            x_change = 0
+
+jump = check_collisions(blocks, jump)
+player_x += x_change
+player_y = update_player(player_y)
+platforms = update_platforms(platforms, player_y, y_change)
+
+if x_change > 0:
+    player = pygame.transform.scale(pygame.image.load('doodle.png'), (90, 70))
+elif x_change < 0:
+    player = pygame.transform.flip(pygame.transform.scale(pygame.image.load('doodle.png'), (90, 70)), 1, 0)
+
+
+pygame.display.flip()
+pygame.quit()
